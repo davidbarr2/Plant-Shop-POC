7f2ef0d9565fba79ffdad1c007fa85103e2ebd83
David - Lambda School User <david-lambdaschooluser@davids-mbp.lan>
2021-10-29 08:50:51 -0400

    completed plant list component

--- src/components/PlantList.js
+++ src/components/PlantList.js
@@ -2,9 +2,15 @@ import React, { Component } from "react";
 import axios from "axios";
 
 export default class PlantList extends Component {
-  // add state with a property called "plants" - initialize as an empty array
+
   state = {
     plants:[]
+  }
+
+  componentDidMount(){
+    axios.get('http://localhost:3333/plants')
+    .then(res => this.setState({plants: res.data}))
+    .catch( err => console.log(err))
   }
   // when the component mounts:
   //   - fetch data from the server endpoint - http://localhost:3333/plants
@@ -12,6 +18,7 @@ export default class PlantList extends Component {
 
   /*********  DON'T CHANGE ANYTHING IN THE RENDER FUNCTION *********/
   render() {
+    console.log(this.state)
     return (
       <main className="plant-list">
         {this.state.plants.map((plant) => (
